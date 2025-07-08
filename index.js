acfbe148ea5bfe414accd1c716bc3dcebfdbd03a
lanalsln <166208409+lanalsln@users.noreply.github.com>
2025-07-08 21:06:50 +0800

    poppin fonts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {SplashScreen} from './src/pages';
+import {Login} from './src/pages';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => SplashScreen);
+AppRegistry.registerComponent(appName, () => Login);
