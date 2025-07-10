3a2b22e8c048ec9e32d629eb0c1551d482d106f1
lanalsln <166208409+lanalsln@users.noreply.github.com>
2025-07-10 08:31:53 +0800

    bottontabs

--- src/components/molecules/buttonnavigation/buttonnavigation.tsx
+++ src/components/molecules/buttonnavigation/buttonnavigation.tsx
@@ -4,18 +4,22 @@ import {AccountIcon, HomeLogo, ActivityLogo, CameraLogo} from '../../atoms';
 
 const Buttonnavigation = () => {
   return (
-    <View>
-      <TouchableOpacity>
-        <AccountIcon />
-      </TouchableOpacity>
-      <TouchableOpacity>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.home}>
         <HomeLogo />
+        <Text style={styles.text}>Home</Text>
       </TouchableOpacity>
-      <TouchableOpacity>
-        <ActivityLogo />
-      </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity style={styles.camera}>
         <CameraLogo />
+        <Text style={styles.text}>Scan</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.activity}>
+        <ActivityLogo />
+        <Text style={styles.text}>History</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.account}>
+        <AccountIcon />
+        <Text style={styles.text}>Account</Text>
       </TouchableOpacity>
     </View>
   );
@@ -23,4 +27,28 @@ const Buttonnavigation = () => {
 
 export default Buttonnavigation;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingVertical: 25,
+
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+
+    backgroundColor: '#FFFFFF',
+
+    borderWidth: 1,
+    borderColor: '#DEDEDE',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  home: {},
+  camera: {},
+  activity: {},
+  account: {},
+  text: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 10,
+  },
+});
